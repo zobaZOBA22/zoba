3f08a3898f2cdd98eb1b3850b57f592664e0bbfe
zobaZOBA22 <74258455+zobaZOBA22@users.noreply.github.com>
2020-11-10 18:13:54 +0200

    Add files via upload

---
+++ zoba22.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char y,z;
+    char A,T;
+    cout<<"enter your depart(A:control,C:computer,T:telecom):";
+    //getline (cin , depart);
+    //z = depart[0];
+    switch (z)
+    {
+    case 'A':
+        depart = 'control';
+        break;
+    case 'T':
+        depart = 'telecom'
+    case 'C':
+        depart = 'computer'
+    
+    default 'error':
+        break;
+    }
+
+
+
+
+
+
+
+
+
+
+}
